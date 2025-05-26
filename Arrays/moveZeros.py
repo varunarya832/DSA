67e54b4c5c3c67e19404f39df66dfbfaaf3ab92c
varun <varunkumararya832@gmail.com>
2025-05-26 17:36:15 +0530

    scaleable

--- Arrays/moveZeros.py
+++ Arrays/moveZeros.py
@@ -7,8 +7,9 @@ def moveZerosToEnd(arr):
     pointer = 0
     for i in range(len(arr)):
         if arr[i] != 0:
-            arr[pointer] = arr[i]
-            pointer +=1
+            if i != pointer:
+                arr[pointer] = arr[i]
+                pointer +=1
         # final temp arr [1,3,12,3,12]
 
     while pointer < len(arr): # 3 < 5 ; 2 runs
